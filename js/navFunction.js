dabfd915c3f6de7d70ee8c13c5e0418fe2a84a9a
1342470 <1342470@stu.ucenmanchester.ac.uk>
2021-12-24 22:29:15 +0000

    various addtion accessabiltty addtions

--- js/navFunction.js
+++ js/navFunction.js
@@ -1,4 +1,6 @@
-function myFunction() {
+document.getElementById("dropDownMenu").addEventListener("click", navMenu);
+
+function navMenu() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
